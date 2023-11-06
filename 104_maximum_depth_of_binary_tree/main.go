9179ef1687e851acf2934b86a69eef9a84b2fbfe
Leon <s8952889@gmail.com>
2023-11-06 18:33:07 +0800

    Update

---
+++ 104_maximum_depth_of_binary_tree/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func main() {
+	root := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 9,
+		},
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val: 15,
+			},
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+	}
+	fmt.Println(maxDepth(root)) // 3
+	root = &TreeNode{
+		Val: 1,
+		Right: &TreeNode{
+			Val: 2,
+		},
+	}
+	fmt.Println(maxDepth(root)) // 2
+	fmt.Println(maxDepth(nil))  // 0
+}
---
+++ 22_generate_parentheses/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func rec(n int, openCount int, closeCount int, current string, result *[]string) {
+	if openCount == n && closeCount == n {
+		*result = append(*result, current)
+		return
+	}
+	if openCount < n {
+		rec(n, openCount+1, closeCount, current+"(", result)
+	}
+	if closeCount < openCount {
+		rec(n, openCount, closeCount+1, current+")", result)
+	}
+}
+
+func generateParenthesis(n int) []string {
+	result := []string{}
+	rec(n, 0, 0, "", &result)
+	return result
+}
+
+func main() {
+	fmt.Println(generateParenthesis(3)) // ["((()))","(()())","(())()","()(())","()()()"]
+	fmt.Println(generateParenthesis(500))
+}
