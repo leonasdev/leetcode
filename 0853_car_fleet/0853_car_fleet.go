c6a0509805a127e3a65600c1f3cabb6504640a5f
Leon <s8952889@gmail.com>
2023-12-28 22:36:58 +0800

    update

--- 0853_car_fleet/0853_car_fleet.go
+++ 0853_car_fleet/0853_car_fleet.go
@@ -10,18 +10,20 @@ type Car struct {
 func carFleet(target int, position []int, speed []int) int {
 	cars := []Car{}
 	for i := 0; i < len(position); i++ {
-		cars = append(cars, Car{position[i], speed[i]})
+		cars = append(cars, Car{
+			Position: position[i],
+			Speed:    speed[i],
+		})
 	}
-
 	sort.Slice(cars, func(i, j int) bool { return cars[i].Position > cars[j].Position })
 
-	slowest := 0.0
+	maxTime := 0.0
 	count := 0
 	for _, car := range cars {
-		carTimeToTarget := float64(target-car.Position) / float64(car.Speed)
-		if carTimeToTarget > slowest {
+		timeToTarget := (float64(target) - float64(car.Position)) / float64(car.Speed)
+		if timeToTarget > maxTime {
 			count++
-			slowest = carTimeToTarget
+			maxTime = timeToTarget
 		}
 	}
 
